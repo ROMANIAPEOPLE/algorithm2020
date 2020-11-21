adb238919dd7edf970c91b4995f66c5f7481a4fc
jeong ki hyuk <jungkh405@naver.com>
2020-11-22 04:32:31 +0900

    게임 개발(시뮬레이션 삼성SW 역량테스트 문제)

--- src/ETC/연습장.java
+++ src/ETC/연습장.java
@@ -1,18 +1,16 @@
 package ETC;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class 연습장  {
 
     public static void main(String[] args) {
 
-
-        String [] arr = {"a"};
-
-        int value = (int)arr[0].charAt(0)-96;
-
-        System.out.println(value);
-
+        System.out.println((0%4));
+        System.out.println((3%4-1));
+        System.out.println((2%4)-1);
+        System.out.println((1%4)-1);
 
     }
 }
