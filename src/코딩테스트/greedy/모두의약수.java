508aaa99e5641216182406d0f46c3c96bb47d6cd
jungkh405@naver.com <jungkh405@naver.com>
2021-01-05 23:54:07 +0900

    모두의약수

--- src/코딩테스트/greedy/모두의약수.java
+++ src/코딩테스트/greedy/모두의약수.java
@@ -1,4 +1,28 @@
 package 코딩테스트.greedy;
 
+import java.util.Scanner;
+
 public class 모두의약수 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int[sc.nextInt()+1];
+
+        for(int i=1; i<arr.length; i++){
+            for(int j=i; j<arr.length; j+=i) {
+                arr[j]++;
+            }
+        }
+
+        for(int i=1; i<arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
 }
+
+/**
+ * 단순 2중포문으로 풀면 시간초과
+ * 풀이
+ * 자기 자신의 약수는 자기 자신이라는 점과, 그 약수의 배수는 모두 약수라는 점을 이해하여 풀이
+ * 1의 약수는 1, 1의 배수는 모두 1을 약수로 가진다.
+ */
