66c8538e4137e6999451127ca969d5e1beb043a4
jungkh405@naver.com <jungkh405@naver.com>
2021-05-02 23:51:44 +0900

    0502

---
+++ src/코드구현력기르기/입력된숫자의개수.java
@@ -0,0 +1,37 @@
+package 코드구현력기르기;
+
+import java.util.Scanner;
+
+public class 입력된숫자의개수 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int value = sc.nextInt();
+
+        int k = 1;
+        int lt = 1;
+        int rt;
+        int cur;
+
+        int result = 0;
+
+        do {
+            lt = n / (k * 10);
+            rt = n % k;
+            cur = (n / k) % k;
+
+            if (cur > value) {
+                result += (lt + 1) * k;
+            } else if (cur == value) {
+                result += (lt * k) + (rt + 1);
+            } else {
+                result += lt * k;
+            }
+
+            k *= 10;
+
+        } while (lt != 0);
+
+        System.out.println(result);
+    }
+}
